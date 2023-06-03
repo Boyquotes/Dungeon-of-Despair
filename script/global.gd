04b189ac61a7c65cbb3582dcdb94bf5b6637c501
Jemuel Morales <moralesjemuel16.jm@gmail.com>
2023-06-03 21:36:35 +0800

    Update global.gd

--- script/global.gd
+++ script/global.gd
@@ -5,6 +5,14 @@ var current_level
 var player_global_position : Vector2
 var player_vec_position : Vector2
 var detected_entities : bool = false
+var current_selected_item : int = 0
+
+#objectives
+var is_door_opened : bool = false
+
+
+#inventory
+
 
 #func _process(_delta: float) -> void:
 #	if player_global_position:
