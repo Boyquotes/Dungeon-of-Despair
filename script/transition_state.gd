6a66a4a8cea1b327bea8c7124dec313c1d1d923e
Jemuel Morales <moralesjemuel16.jm@gmail.com>
2023-06-03 21:36:39 +0800

    Update transition_state.gd

--- script/transition_state.gd
+++ script/transition_state.gd
@@ -4,13 +4,22 @@ extends CanvasLayer
 
 enum transition {
 	TRANSITION_OUT,
-	TRANSITION_IN
+	TRANSITION_IN,
+	LETTERBOXING_IN,
+	LETTERBOXING_OUT
 }
 func ChangeScenes(transitions):
 	if transitions != null:
+		#transitions in and out
 		if transitions == transition.TRANSITION_IN:
 			animation_player.play("transition_in")
-			await animation_player.animation_finished
 		elif transitions == transition.TRANSITION_OUT:
 			animation_player.play("transition_out")
-			await animation_player.animation_finished
+		
+		#transitions for letterboxing / black_bar
+		if transitions == transition.LETTERBOXING_IN:
+			animation_player.play("letterboxing_in")
+		elif transitions == transition.LETTERBOXING_OUT:
+			animation_player.play("letterboxing_out")
+		#thread if finished read next line of code
+		await animation_player.animation_finished
